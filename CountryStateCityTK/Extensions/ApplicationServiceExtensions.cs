45ae9982b3b671b5825f121dbc364e6604e3a859
PrateekPushpad <prateekpushpad19@gmail.com>
2024-01-27 23:19:25 +0530

    Add Extension method

---
+++ CountryStateCityTK/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using DAL;
+using DAL.Implementations;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace UI.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("UI")));
+
+            services.AddScoped<ICountryRepo, CountryRepo>();
+            services.AddScoped<IStateRepo, StateRepo>();
+            services.AddScoped<ICityRepo, CityRepo>();
+            services.AddScoped<ISkillRepo, SkillRepo>();
+            services.AddScoped<IStudentRepo, StudentRepo>();
+            services.AddScoped<IUserInfoRepo, UserInfoRepo>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            return services;
+        }
+    }
+}
--- CountryStateCityTK/Program.cs
+++ CountryStateCityTK/Program.cs
@@ -2,19 +2,14 @@ using DAL;
 using DAL.Implementations;
 using DAL.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using UI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("UI")));
-builder.Services.AddScoped<ICountryRepo, CountryRepo>();
-builder.Services.AddScoped<IStateRepo, StateRepo>();
-builder.Services.AddScoped<ICityRepo, CityRepo>();
-builder.Services.AddScoped<ISkillRepo, SkillRepo>();
-builder.Services.AddScoped<IStudentRepo, StudentRepo>();
-builder.Services.AddScoped<IUserInfoRepo, UserInfoRepo>();
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddApplicationServices(builder.Configuration);
 
 builder.Services.AddSession(options =>
 {
